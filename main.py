2420d0cdad237de0860d876290ac4cb47e5f1736
stepanpopov <stepan@DESKTOP-81FUGHF>
2022-09-15 11:41:25 +0300

    done

--- main.py
+++ main.py
@@ -1,59 +1,53 @@
 import sys
-import math
+import math       
 
 def get_coef(index, prompt):
-    '''
-    Читаем коэффициент из командной строки или вводим с клавиатуры
-
-    Args:
-        index (int): Номер параметра в командной строке
-        prompt (str): Приглашение для ввода коэффицента
-
-    Returns:
-        float: Коэффициент квадратного уравнения
-    '''
     try:
-        # Пробуем прочитать коэффициент из командной строки
         coef_str = sys.argv[index]
     except:
-        # Вводим с клавиатуры
         print(prompt)
         coef_str = input()
+
     # Переводим строку в действительное число
-    coef = float(coef_str)
+    try:
+        coef = float(coef_str)
+    except:
+        coef_str = input()
+        while True:
+            try:
+                coef = float(coef_str)
+                break
+            except:
+                coef_str = input()
+
     return coef
 
+def t_to_x(t):
+    x_list = []
+    if t == 0:
+        x_list.append(0)
+    elif t > 0:
+        sq_t = math.sqrt(t)
+        x_list.append(sq_t)
+        x_list.append((-1) * sq_t)
+    return x_list
 
 def get_roots(a, b, c):
-    '''
-    Вычисление корней квадратного уравнения
-
-    Args:
-        a (float): коэффициент А
-        b (float): коэффициент B
-        c (float): коэффициент C
-
-    Returns:
-        list[float]: Список корней
-    '''
     result = []
-    D = b*b - 4*a*c
-    if D == 0.0:
-        root = -b / (2.0*a)
-        result.append(root)
-    elif D > 0.0:
-        sqD = math.sqrt(D)
-        root1 = (-b + sqD) / (2.0*a)
-        root2 = (-b - sqD) / (2.0*a)
-        result.append(root1)
-        result.append(root2)
+    D_t = b*b - 4*a*c
+    if D_t == 0.0:
+        t = -b / (2.0*a)
+        result += t_to_x(t) 
+    elif D_t > 0:
+        sq_D = math.sqrt(D_t)
+        t1 = (-b + sq_D) / (2.0*a)
+        t2 = (-b - sq_D) / (2.0*a)
+        result += t_to_x(t1)
+        result += t_to_x(t2)
     return result
 
 
 def main():
-    '''
-    Основная функция
-    '''
     a = get_coef(1, 'Введите коэффициент А:')
     b = get_coef(2, 'Введите коэффициент B:')
     c = get_coef(3, 'Введите коэффициент C:')
@@ -61,12 +55,15 @@ def main():
     roots = get_roots(a,b,c)
     # Вывод корней
     len_roots = len(roots)
+
+    print()
     if len_roots == 0:
         print('Нет корней')
-    elif len_roots == 1:
-        print('Один корень: {}'.format(roots[0]))
-    elif len_roots == 2:
-        print('Два корня: {} и {}'.format(roots[0], roots[1]))
+    else:
+        print("Количество корней {}".format(len_roots))
+        for root in roots:
+            print(round(root, 4), end = ' ')
+        print()
 
 
 # Если сценарий запущен из командной строки
